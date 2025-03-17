ef3ae62589ec70df924abc0945dccd87d0d269ff
waleed0z <waleedashir6@gmail.com>
2025-03-17 17:41:07 +0000

    oauth

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")  # Change this to a secure random string
     GOOGLE_CLIENT_ID = os.getenv("CLIENT_ID")
-    GOOGLE_CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+    GOOGLE_CLIENT_SECRET = os.getenv("C_S")
     GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
