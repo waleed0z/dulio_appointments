2397023ac38fc4b6bedcf87707cb17d46fff4d49
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-06-15 12:24:26 +0100

    deploy

--- app/__init__.py
+++ app/__init__.py
@@ -11,12 +11,13 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
     db.init_app(app)
     login_manager.init_app(app)
     migrate.init_app(app, db)
-
     from .routes import register_routes
     register_routes(app)
     from . import models
     return app
+
+app = create_app()
+
